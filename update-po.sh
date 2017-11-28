82e9fb7115bf651cf54c7768ba748b216ca5ed6b
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2017-11-28 14:52:54 +0100

    Update files update-po(t).sh, includes added license headers.

--- update-po.sh
+++ update-po.sh
@@ -2,8 +2,28 @@
 
 set -x
 
+# Copyright (C) 2017 by Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
 GETTEXT_DOMAIN=$(cat configure.ac | grep -E "^GETTEXT_PACKAGE=" | sed -e 's/GETTEXT_PACKAGE=//')
 
 cd po/
-cat LINGUAS | while read lingua; do touch ${lingua}.po; intltool-update --gettext-package ${GETTEXT_DOMAIN} $(basename ${lingua}); done
+cat LINGUAS | while read lingua; do
+	if [ ! -e ${lingua}.po ]; then
+		msginit --input=${GETTEXT_DOMAIN}.pot --locale=${lingua} --no-translator --output-file=$lingua.po
+	else
+		intltool-update --gettext-package ${GETTEXT_DOMAIN} $(basename ${lingua})
+	fi
+done
 cd - 1>/dev/null
--- update-pot.sh
+++ update-pot.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright (C) 2017 by Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
 GETTEXT_DOMAIN=$(cat configure.ac | grep -E "^GETTEXT_PACKAGE=" | sed -e 's/GETTEXT_PACKAGE=//')
 
 cd po/ && intltool-update --gettext-package ${GETTEXT_DOMAIN} --pot
