ArcticaProject/arctica-greeter

commit 32d28d7bf2646fc7a0008937034246fcc96dbc8a

misc src/: make ArcticaGreeter a proper vala SingleInstance class.
This allows us to drop the rather awkward self-referencing static
singleton member and use a standard vala/glib feature.

src/arctica-greeter.vala
@@ -21,16 +21,15 @@
 
 public const int grid_size = 40;
 
-public class ArcticaGreeter
+[SingleInstance]
+public class ArcticaGreeter : Object
 {
-    public static ArcticaGreeter singleton;
-
     public signal void show_message (string text, LightDM.MessageType type);
     public signal void show_prompt (string text, LightDM.PromptType type);
     public signal void authentication_complete ();
     public signal void starting_session ();
 
-    public bool test_mode = false;
+    public bool test_mode { get; construct; default = false; }
 
     private string state_file;
     private KeyFile state;
@@ -53,11 +52,8 @@ public class ArcticaGreeter
     public signal void xsettings_ready ();
     public signal void greeter_ready ();
 
-    private ArcticaGreeter (bool test_mode_)
+    construct
     {
-        singleton = this;
-        test_mode = test_mode_;
-
         greeter = new LightDM.Greeter ();
         greeter.show_message.connect ((text, type) => { show_message (text, type); });
         greeter.show_prompt.connect ((text, type) => { show_prompt (text, type); });
@@ -138,6 +134,22 @@ public class ArcticaGreeter
         }
         else
             xsettings_ready_cb ();
+    }
+
+    /*
+     * Note that we need a way to specify a parameter for the initial instance
+     * creation of the singleton, but also a constructor that takes no
+     * parameters for later usage.
+     *
+     * Making the parameter optional is a good compromise.
+     *
+     * This this parameter is construct-only, initializing it by passing it to
+     * the GObject constructor is both the correct way to do it, and it will
+     * additionally avoid changing it in later calls of our constructor.
+     */
+    public ArcticaGreeter (bool test_mode_ = false)
+    {
+        Object (test_mode: test_mode_);
     }
 
     public string? get_state (string key)
@@ -340,7 +352,7 @@ public class ArcticaGreeter
     {
         try
         {
-            ArcticaGreeter.singleton.greeter.authenticate_remote (session, userid);
+            greeter.authenticate_remote (session, userid);
         }
         catch (Error e)
         {

tests/arctica-greeter.vala
@@ -19,7 +19,8 @@
 
 public const int grid_size = 40;
 
-public class ArcticaGreeter
+[SingleInstance]
+public class ArcticaGreeter : Object
 {
     public static ArcticaGreeter singleton;
 
@@ -27,10 +28,15 @@ public class ArcticaGreeter
     public signal void show_prompt (string text, LightDM.PromptType type);
     public signal void authentication_complete ();
 
-    public bool test_mode = false;
+    public bool test_mode { get; construct; default = false; }
     public bool session_started = false;
     public string last_respond_response;
     public bool orca_needs_kick;
+
+    public ArcticaGreeter (bool test_mode_ = false)
+    {
+        Object (test_mode: test_mode_);
+    }
 
     public bool is_authenticated ()
     {

src/background.vala
@@ -778,7 +778,8 @@ public class Background : Gtk.Fixed
     {
         notify_property ("average-color");
 
-        if (!ArcticaGreeter.singleton.test_mode)
+        var greeter = new ArcticaGreeter ();
+        if (!greeter.test_mode)
         {
             var rgba = current.average_color.to_string ();
             var root = get_screen ().get_root_window ();

src/dash-box.vala
@@ -54,7 +54,8 @@ public class DashBox : Gtk.Box
     /* Does not actually add w to this widget, as doing so would potentially mess with w's placement. */
     public void set_base (Gtk.Widget? w)
     {
-        if (!ArcticaGreeter.singleton.test_mode) {
+        var greeter = new ArcticaGreeter ();
+        if (!greeter.test_mode) {
             return_if_fail (pushed == null);
             return_if_fail (mode == Mode.NORMAL);
         }

src/dash-entry.vala
@@ -301,10 +301,11 @@ public class DashEntry : Gtk.Entry, Fadable
         // This is a workaround for bug https://launchpad.net/bugs/944159
         // The problem is that orca seems to not notice that it's in a password
         // field on startup.  We just need to kick orca in the pants.
-        if (ArcticaGreeter.singleton.orca_needs_kick)
+        var greeter = new ArcticaGreeter ();
+        if (greeter.orca_needs_kick)
         {
             Signal.emit_by_name (get_accessible (), "focus-event", true);
-            ArcticaGreeter.singleton.orca_needs_kick = false;
+            greeter.orca_needs_kick = false;
         }
 
         return base.key_press_event (event);

src/greeter-list.vala
@@ -229,7 +229,8 @@ public abstract class GreeterList : FadableBox
 
     public void cancel_authentication ()
     {
-        ArcticaGreeter.singleton.cancel_authentication ();
+        var greeter = new ArcticaGreeter ();
+        greeter.cancel_authentication ();
         entry_selected (selected_entry.id);
     }
 
@@ -483,7 +484,8 @@ public abstract class GreeterList : FadableBox
         entry.destroy ();
 
         /* Show a manual login if no users and no remote login entry */
-        if (!have_entries () && !ArcticaGreeter.singleton.show_remote_login_hint ())
+        var greeter = new ArcticaGreeter ();
+        if (!have_entries () && !greeter.show_remote_login_hint ())
             add_manual_entry ();
 
         queue_draw ();
@@ -793,9 +795,10 @@ public abstract class GreeterList : FadableBox
 
     protected void connect_to_lightdm ()
     {
-        ArcticaGreeter.singleton.show_message.connect (show_message_cb);
-        ArcticaGreeter.singleton.show_prompt.connect (show_prompt_cb);
-        ArcticaGreeter.singleton.authentication_complete.connect (authentication_complete_cb);
+        var greeter = new ArcticaGreeter ();
+        greeter.show_message.connect (show_message_cb);
+        greeter.show_prompt.connect (show_prompt_cb);
+        greeter.authentication_complete.connect (authentication_complete_cb);
     }
 
     protected void show_message_cb (string text, LightDM.MessageType type)
@@ -809,10 +812,11 @@ public abstract class GreeterList : FadableBox
         /* Notify the greeter on what user has been logged */
         if (get_selected_id () == "*other" && manual_name == null)
         {
-            if (ArcticaGreeter.singleton.test_mode)
+            var greeter = new ArcticaGreeter ();
+            if (greeter.test_mode)
                 manual_name = test_username;
             else
-                manual_name = ArcticaGreeter.singleton.authentication_user();
+                manual_name = greeter.authentication_user();
         }
 
         prompted = true;
@@ -841,10 +845,11 @@ public abstract class GreeterList : FadableBox
             return;
 
         bool is_authenticated;
-        if (ArcticaGreeter.singleton.test_mode)
+        var greeter = new ArcticaGreeter ();
+        if (greeter.test_mode)
             is_authenticated = test_is_authenticated;
         else
-            is_authenticated = ArcticaGreeter.singleton.is_authenticated();
+            is_authenticated = greeter.is_authenticated();
 
         if (is_authenticated)
         {
@@ -852,7 +857,7 @@ public abstract class GreeterList : FadableBox
             if (prompted && !unacknowledged_messages)
             {
                 login_complete ();
-                if (ArcticaGreeter.singleton.test_mode)
+                if (greeter.test_mode)
                     start_session ();
                 else
                 {
@@ -905,16 +910,17 @@ public abstract class GreeterList : FadableBox
 
         greeter_authenticating_user = get_selected_id ();
 
-        if (ArcticaGreeter.singleton.test_mode)
+        var greeter = new ArcticaGreeter ();
+        if (greeter.test_mode)
             test_start_authentication ();
         else
         {
             if (get_selected_id () == "*other")
-                ArcticaGreeter.singleton.authenticate ();
+                greeter.authenticate ();
             else if (get_selected_id () == "*guest")
-                ArcticaGreeter.singleton.authenticate_as_guest ();
+                greeter.authenticate_as_guest ();
             else
-                ArcticaGreeter.singleton.authenticate (get_selected_id ());
+                greeter.authenticate (get_selected_id ());
         }
     }
 
@@ -929,7 +935,8 @@ public abstract class GreeterList : FadableBox
 
     private void start_session ()
     {
-        if (!ArcticaGreeter.singleton.start_session (get_lightdm_session (), background))
+        var greeter = new ArcticaGreeter ();
+        if (!greeter.start_session (get_lightdm_session (), background))
         {
             show_message (_("Failed to start session"), true);
             start_authentication ();

src/main-window.vala
@@ -159,7 +159,8 @@ public class MainWindow : Gtk.Window
         only_on_monitor = AGSettings.get_string(AGSettings.KEY_ONLY_ON_MONITOR);
         monitor_setting_ok = only_on_monitor == "auto";
 
-        if (ArcticaGreeter.singleton.test_mode)
+        var greeter = new ArcticaGreeter ();
+        if (greeter.test_mode)
         {
             /* Simulate an 800x600 monitor to the left of a 640x480 monitor */
             monitors = new List<Monitor> ();
@@ -375,6 +376,7 @@ public class MainWindow : Gtk.Window
             }
         }
 
+        var greeter = new ArcticaGreeter ();
         switch (event.keyval)
         {
         case Gdk.Key.Escape:
@@ -434,14 +436,14 @@ public class MainWindow : Gtk.Window
             }
             return true;
         case Gdk.Key.z:
-            if (ArcticaGreeter.singleton.test_mode && (event.state & Gdk.ModifierType.MOD1_MASK) != 0)
+            if (greeter.test_mode && (event.state & Gdk.ModifierType.MOD1_MASK) != 0)
             {
                 show_shutdown_dialog (ShutdownDialogType.SHUTDOWN);
                 return true;
             }
             break;
         case Gdk.Key.Z:
-            if (ArcticaGreeter.singleton.test_mode && (event.state & Gdk.ModifierType.MOD1_MASK) != 0)
+            if (greeter.test_mode && (event.state & Gdk.ModifierType.MOD1_MASK) != 0)
             {
                 show_shutdown_dialog (ShutdownDialogType.RESTART);
                 return true;

src/menubar.vala
@@ -108,7 +108,8 @@ public class MenuBar : Gtk.MenuBar
      */
     public void set_keyboard_state ()
     {
-        if (!ArcticaGreeter.singleton.test_mode)
+        var greeter = new ArcticaGreeter ();
+        if (!greeter.test_mode)
             onscreen_keyboard_item.set_active (AGSettings.get_boolean (AGSettings.KEY_ONSCREEN_KEYBOARD));
     }
 
@@ -155,7 +156,8 @@ public class MenuBar : Gtk.MenuBar
 
         setup_indicators ();
 
-        ArcticaGreeter.singleton.starting_session.connect (cleanup);
+        var greeter = new ArcticaGreeter ();
+        greeter.starting_session.connect (cleanup);
     }
 
     private void close_pid (ref Pid pid)
@@ -293,7 +295,8 @@ public class MenuBar : Gtk.MenuBar
 
     private void load_indicator (string indicator_name)
     {
-        if (!ArcticaGreeter.singleton.test_mode)
+        var greeter = new ArcticaGreeter ();
+        if (!greeter.test_mode)
         {
             if (indicator_name == "ug-accessibility")
             {
@@ -484,7 +487,8 @@ public class MenuBar : Gtk.MenuBar
                 // this is not racy with orca startup, it is racy with whether
                 // orca will read the first character or not out loud.  Hence
                 // why we do both.  Ideally this would be fixed in orca itself.
-                ArcticaGreeter.singleton.orca_needs_kick = true;
+                var greeter = new ArcticaGreeter ();
+                greeter.orca_needs_kick = true;
                 Timeout.add_seconds (1, () => {
                     Signal.emit_by_name ((get_toplevel () as Gtk.Window).get_focus ().get_accessible (), "focus-event", true);
                     return false;

commit 30035fe8aa2d388b2493cafc4f5b17755b99a0f1

src/arctica-greeter.vala: fix test mode.
32d28d7bf2646fc7a0008937034246fcc96dbc8a broke test mode by changing the
ArcticaGreeter class to a proper SingleInstance vala class. While meant
well, this created deadlocks, especially when using test mode, since
other code being called from the ArcticaGreeter constructor tries to
acquire references to ArcticaGreeter, which is still locked at that
point in time.

Fortunately, GObject has the constructed () function, that is almost
never used within vala, but still works and is called after the
constructor () (or, in vala parlance, construct), so we can move calling
functions that might require a constructed ArcticaGreeter to constructed
().

Fixes: https://github.com/ArcticaProject/arctica-greeter/issues/42

src/arctica-greeter.vala
@@ -33,6 +33,7 @@ public class ArcticaGreeter : Object
 
     private string state_file;
     private KeyFile state;
+    private bool continue_init = false;
 
     private Cairo.XlibSurface background_surface;
 
@@ -104,9 +105,10 @@ public class ArcticaGreeter : Object
                 warning ("Failed to load state from %s: %s\n", state_file, e.message);
         }
 
+        /* Render things after xsettings is ready */
+        xsettings_ready.connect ( xsettings_ready_cb );
+
         if (!test_mode) {
-            /* Render things after xsettings is ready */
-            xsettings_ready.connect ( xsettings_ready_cb );
 
             GLib.Bus.watch_name (BusType.SESSION, "org.mate.SettingsDaemon", BusNameWatcherFlags.NONE,
                                  (c, name, owner) =>
@@ -119,7 +121,7 @@ public class ArcticaGreeter : Object
                                             {
                                                 if (name == "xsettings") {
                                                     debug ("xsettings is ready");
-                                                    xsettings_ready ();
+                                                    continue_init = true;
                                                 }
                                             }
                                         );
@@ -127,13 +129,43 @@ public class ArcticaGreeter : Object
                                     catch (Error e)
                                     {
                                         debug ("Failed to get MSD proxy, proceed anyway");
-                                        xsettings_ready ();
+                                        continue_init = true;
                                     }
                                 },
                                 null);
         }
         else
-            xsettings_ready_cb ();
+        {
+            /*
+             * Since this is now a proper SingleInstance class, we have to
+             * make sure to finish constructing as early as possible.
+             *
+             * Calling xsettings_ready_cb () here is *not* possible, since the
+             * function calls a lot of other things that eventually need a
+             * reference to ArcticaGreeter, but since we're still constructing
+             * it, fetching a reference would just deadlock.
+             *
+             * Fixing this isn't really easy. We cannot use timeouts, since we
+             * don't have a main loop available at that point in time.
+             *
+             * Fortunately, GObject's initialization sequence is quite
+             * sophisticated and provides a way to run code after the
+             * constructor function (i.e., this one) has finished - the
+             * virtual constructed function.
+             */
+            continue_init = true;
+        }
+    }
+
+    public override void constructed ()
+    {
+        if (continue_init)
+        {
+            xsettings_ready ();
+        }
+
+        /* Chain up - actually necessary. */
+        base.constructed ();
     }
 
     /*